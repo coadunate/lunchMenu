9cf8d3d05461509eade6d7f0c47f1ba729802d36
Bryn Ready (bwr040) <bryn.ready@usask.ca>
2017-05-09 17:19:17 -0600

    updated to include parquis, not complated yet

--- menu.py
+++ menu.py
@@ -1,9 +1,20 @@
-#!/usr/env python 
+#!/usr/bin/env python 
 from bs4 import *
 import requests
+import argparse 
+
+parser = argparse.ArgumentParser() 
+
+parser.add_argument("-l", "--location", action="store", help = "specify location as ag or marquis", default="ag", dest="loc") 
+
+args=parser.parse_args()
+if("ag" in args.loc.lower() ): 
+  myUrl="http://www.usask.ca/culinaryservices/agriculture-menu.php"
+else: 
+  myUrl="http://www.usask.ca/culinaryservices/marquis-menu.php"
 
 #use the request package to make the request
-menu = requests.get("http://www.usask.ca/culinaryservices/agriculture-menu.php")
+menu = requests.get(myUrl)
 
 #check that the request worked
 #print(menu.text)
